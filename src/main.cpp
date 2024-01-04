812c9a3c045dcad9bfb13104f1b092e239c7822c
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-01-04 19:26:57 +0700

    big changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <string>
+#include <ranges>
+#include <algorithm>
+
 #include <boost/asio.hpp>
+#include <fmt/format.h>
+
+using namespace boost::asio;
 
 int main()
 {
-    std::cout << "Hello asio 0_o\n";
+    auto ctx = io_context{};
+
+    while(true)
+    {
+        try 
+        {
+            auto ep = ip::tcp::endpoint(ip::tcp::v4(), 88);
+            auto acceptor = ip::tcp::acceptor(ctx, ep);
+            auto sock = ip::tcp::socket(ctx);
+            acceptor.accept(sock);
+            
+            auto buf = streambuf{};
+            auto bytes_count = read_until(sock, buf, '\n');
+
+            auto str = std::string(std::istreambuf_iterator<char>(&buf), {});
+            fmt::println("Reflected {} bytes: {}", bytes_count - 1, str);
+            
+            write(sock, buf);
+            sock.close();
+
+            //auto str_lower = 
+            //    str 
+            //    | std::views::take(bytes_count - 1) 
+            //    | std::views::transform([](auto&& s) { return std::tolower(s); });
+
+            if(/*std::ranges::equal("stop", str_lower) ||*/ "stop\n" == str)
+            {
+                break;
+            }
+        }
+        catch(std::exception& ex)
+        {
+            fmt::println("{}", ex.what());
+        }
+    }
+
     return 0;
 }
