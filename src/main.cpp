252c0f551fc86a0e3695400fb73320d0524266ac
zdrvzbl <sanya2003ogorodov@gmail.com>
2024-01-05 14:50:11 +0700

    .

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 
 #include <boost/asio.hpp>
+#include <boost/algorithm/string.hpp>
 #include <fmt/format.h>
 
 using namespace boost::asio;
@@ -11,40 +12,41 @@ using namespace boost::asio;
 int main()
 {
     auto ctx = io_context{};
+    auto ep = ip::tcp::endpoint(ip::tcp::v4(), 80);
+    auto acceptor = ip::tcp::acceptor(ctx, ep);
+    fmt::println("Server running");
 
-    while(true)
+    try 
     {
-        try 
+        auto sock = ip::tcp::socket(ctx);
+        acceptor.accept(sock);
+        fmt::println("Connection established");
+        
+        auto buf = streambuf{};
+        auto should_stop = false;
+        while(!should_stop) 
         {
-            auto ep = ip::tcp::endpoint(ip::tcp::v4(), 88);
-            auto acceptor = ip::tcp::acceptor(ctx, ep);
-            auto sock = ip::tcp::socket(ctx);
-            acceptor.accept(sock);
-            
-            auto buf = streambuf{};
             auto bytes_count = read_until(sock, buf, '\n');
-
             auto str = std::string(std::istreambuf_iterator<char>(&buf), {});
-            fmt::println("Reflected {} bytes: {}", bytes_count - 1, str);
+            fmt::println("Recieved {} bytes: {}", bytes_count, str);
+
+            boost::to_lower(str);
+            boost::trim(str);
+            should_stop = (str == "stop");
             
-            write(sock, buf);
-            sock.close();
-
-            //auto str_lower = 
-            //    str 
-            //    | std::views::take(bytes_count - 1) 
-            //    | std::views::transform([](auto&& s) { return std::tolower(s); });
-
-            if(/*std::ranges::equal("stop", str_lower) ||*/ "stop\n" == str)
-            {
-                break;
-            }
+            str.append("::exo\n");
+            fmt::println("Sending answer: {}", str);
+            write(sock, buffer(str));
         }
-        catch(std::exception& ex)
-        {
-            fmt::println("{}", ex.what());
-        }
+        sock.close();
+        fmt::println("Connection closed");
     }
+    catch(std::exception& ex)
+    {
+        fmt::println("{}", ex.what());
+    }
+
+    fmt::println("Server stopped");
 
     return 0;
 }
